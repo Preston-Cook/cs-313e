c81d68189edbd418442f96e9951f622c031a3504
Preston-Cook <preston.l.cook@gmail.com>
2024-01-21 10:39:32 -0600

    fix variable names

--- assignment1/spiral.py
+++ assignment1/spiral.py
@@ -1,7 +1,8 @@
 """
     File: spiral.py
-    Description: A program that generates a spiral matrix beginning at the center and traveling right, left, down, and up while incrementing the previous value in the spiral by one. 
-                 The sum_sub_grid function sums adjacent numbers given a value in the matrix spiral. If the value does not exist, then a 0 is returned
+    Description: A program that generates a spiral matrix beginning at the center and traveling right, 
+                left, down, and up while incrementing the previous value in the spiral by one. 
+                The sum_sub_grid function sums adjacent numbers given a value in the matrix spiral. If the value does not exist, then a 0 is returned
     Student Name: Preston Cook
     Student UT EID: plc886
     Partner Name: Crystal Hicks
@@ -38,7 +39,9 @@ def create_spiral(dim):
 
         if idx == 0:
             # right
-            while x_val < mult + prev_x and x_val != dim - 1: # prevent spillover in final pass
+
+            # prevent spillover in final pass
+            while x_val < mult + prev_x and x_val != dim - 1: 
                 cur += 1
                 x_val += 1
                 matrix[y_val][x_val] = cur
@@ -79,10 +82,10 @@ def sum_sub_grid(grid, val):
     surrounding the parameter val in the grid
     if val is out of bounds, returns 0
     """
-    n = len(grid)
+    n_val = len(grid)
 
-    for row in range(n):
-        for col in range(n):
+    for row in range(n_val):
+        for col in range(n_val):
             if grid[row][col] == val:
                 total = 0
 
@@ -95,7 +98,7 @@ def sum_sub_grid(grid, val):
                     total += grid[row - 1][col]
 
                 # top-right
-                if row - 1 >= 0 and col + 1 < n:
+                if row - 1 >= 0 and col + 1 < n_val:
                     total += grid[row - 1][col + 1]
 
                 # mid-left
@@ -103,19 +106,19 @@ def sum_sub_grid(grid, val):
                     total += grid[row][col - 1]
 
                 # mid-right
-                if col + 1 < n:
+                if col + 1 < n_val:
                     total += grid[row][col + 1]
 
                 # bottom-left
-                if row + 1 < n and col - 1 >= 0:
+                if row + 1 < n_val and col - 1 >= 0:
                     total += grid[row + 1][col - 1]
 
                 # bottom-mid
-                if row + 1 < n:
+                if row + 1 < n_val:
                     total += grid[row + 1][col]
 
                 # bottom-right
-                if row + 1 < n and col + 1 < n:
+                if row + 1 < n_val and col + 1 < n_val:
                     total += grid[row + 1][col + 1]
 
                 return total
