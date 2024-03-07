774b5f2ed4df2c50b8535c093bd4c9ea261ce978
Boltcry <crystal.hicks.r@gmail.com>
2024-03-06 22:46:26 -0600

    added class activity 5
    txt file is question 2, .py file is question 3

---
+++ class-activities/activity5/median.py
@@ -0,0 +1,23 @@
+import heapq 
+
+
+def heapsort(list):
+    heap = []
+    for item in list:
+        heapq.heappush(heap, item)
+    return [heapq.heappop(heap) for i in range(len(heap))]
+
+
+def get_median(m_list):
+    sorted_list = heapsort(m_list)
+
+    mid_index = len(sorted_list) // 2
+    median = sorted_list[mid_index]
+    if len(sorted_list) % 2 == 0:
+        median = (sorted_list[mid_index-1] + sorted_list[mid_index]) / 2
+
+    return median
+
+test_list = [4, 5, 8, 9, 10]
+print(get_median(test_list))
+
